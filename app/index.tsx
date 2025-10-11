cb12ba450bd9e5877248a302f451d5eb99f64195
Ebrahim Buceta <ebrahimbuce@gmail.com>
2025-10-11 17:34:31 -0400

    initialize project with @react-native-reusables/cli

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
+import { UserMenu } from '@/components/user-menu';
+import { useUser } from '@clerk/clerk-expo';
 import { Link, Stack } from 'expo-router';
-import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
+import { MoonStarIcon, XIcon, SunIcon } from 'lucide-react-native';
 import { useColorScheme } from 'nativewind';
 import * as React from 'react';
 import { Image, type ImageStyle, View } from 'react-native';
@@ -12,43 +14,54 @@ const LOGO = {
   dark: require('@/assets/images/react-native-reusables-dark.png'),
 };
 
-const SCREEN_OPTIONS = {
-  title: 'React Native Reusables',
-  headerTransparent: true,
-  headerRight: () => <ThemeToggle />,
+const CLERK_LOGO = {
+  light: require('@/assets/images/clerk-logo-light.png'),
+  dark: require('@/assets/images/clerk-logo-dark.png'),
 };
 
-const IMAGE_STYLE: ImageStyle = {
-  height: 76,
-  width: 76,
+const LOGO_STYLE: ImageStyle = {
+  height: 36,
+  width: 40,
+};
+
+const SCREEN_OPTIONS = {
+  header: () => (
+    <View className="top-safe absolute left-0 right-0 flex-row justify-between px-4 py-2 web:mx-2">
+      <ThemeToggle />
+      <UserMenu />
+    </View>
+  ),
 };
 
 export default function Screen() {
   const { colorScheme } = useColorScheme();
+  const { user } = useUser();
 
   return (
     <>
       <Stack.Screen options={SCREEN_OPTIONS} />
       <View className="flex-1 items-center justify-center gap-8 p-4">
-        <Image source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} resizeMode="contain" />
-        <View className="gap-2 p-4">
-          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
-            1. Edit <Text variant="code">app/index.tsx</Text> to get started.
+        <View className="flex-row items-center justify-center gap-3.5">
+          <Image
+            source={CLERK_LOGO[colorScheme ?? 'light']}
+            resizeMode="contain"
+            style={LOGO_STYLE}
+          />
+          <Icon as={XIcon} className="mr-1 size-5" />
+          <Image source={LOGO[colorScheme ?? 'light']} style={LOGO_STYLE} resizeMode="contain" />
+        </View>
+        <View className="max-w-sm gap-2 px-4">
+          <Text variant="h1" className="text-3xl font-medium">
+            Make it yours{user?.firstName ? `, ${user.firstName}` : ''}.
           </Text>
-          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
-            2. Save to see your changes instantly.
+          <Text className="ios:text-foreground text-center font-mono text-sm text-muted-foreground">
+            Update the screens and components to match your design and logic.
           </Text>
         </View>
-        <View className="flex-row gap-2">
-          <Link href="https://reactnativereusables.com" asChild>
-            <Button>
-              <Text>Browse the Docs</Text>
-            </Button>
-          </Link>
-          <Link href="https://github.com/founded-labs/react-native-reusables" asChild>
-            <Button variant="ghost">
-              <Text>Star the Repo</Text>
-              <Icon as={StarIcon} />
+        <View className="gap-2">
+          <Link href="https://go.clerk.com/8e6CCee" asChild>
+            <Button size="sm">
+              <Text>Explore Clerk Docs</Text>
             </Button>
           </Link>
         </View>
@@ -66,12 +79,8 @@ function ThemeToggle() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
   return (
-    <Button
-      onPressIn={toggleColorScheme}
-      size="icon"
-      variant="ghost"
-      className="ios:size-9 rounded-full web:mx-4">
-      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
+    <Button onPress={toggleColorScheme} size="icon" variant="ghost" className="rounded-full">
+      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-6" />
     </Button>
   );
 }
